fbc70717195a4e1fe6f27a74ce7ee7f9931fa4a3
neelm88 <32981489+neelm88@users.noreply.github.com>
2018-12-27 10:51:26 -0500

    Update WordSet.py

--- WordSet.py
+++ WordSet.py
@@ -2,7 +2,13 @@ from Word import Word
 class WordSet:
     def __init__(self):
         self.elements = []
+#
+#Class Methods        
+#
 
+#Moves the item in elements that is word, and puts it in the front of the list elements.
+#   elements: This is the list of Word Objects being manipulated 
+#   word: The Word object being searched for.
     def moveToFront(elements, word):
         found = False
         for x in elements:
@@ -10,6 +16,10 @@ class WordSet:
                 found = True
                 elements.remove(word)
                 elements.insert(0,word)
+                               
+#Returns, but does not remove the Word Object from elements that is the string str.
+#   elements: The list of Word Objects being searched.
+#   str: The string that is contained in one of the Word Objects in elements.
     def showWord(elements, str):
         found = False
         output = Word(None,None,None)
@@ -18,24 +28,40 @@ class WordSet:
                 output = x
                 found = True
         return output
+
+#    
+#Instance Methods   
+#    
+        
+#Adds the Word Object to self. 
+#   word: A Word Object being added to self.
     def add(self, word):
         self.elements.append(word)
+        
+#Removes and returns the Word Object from self.        
+#   word: A Word Object being removed from self.        
     def remove(self, word):
         WordSet.moveToFront(self.elements, word)
         return self.pop(0)
+    
+#Returns true if there is a word with the string str in self, false otherwise.
+#   str: The string that is being searched for in self.
     def contains(self, str):
         found = False
         for x in self.elements:
             if (x.word == str and not found):
                 found = True
         return found
+    
+#Removes and returns one item from self.
     def removeAny(self):
         return self.elements.pop(0)
+    
+#Returns the size of self.
     def size(self):
         return len(self.elements)
 
-    # Returns the Word Objecf from Word_List that has the highest count.
-    #   Word_List: The list which stores Word Objects.
+# Returns the Word Object from self that has the highest count.
     def max(self):
         top = 0
         output = ""
@@ -44,6 +70,8 @@ class WordSet:
                 top = x.count
                 output = x
         return output
+#Increases the count of a Word Object in self that has the word value of str.    
+#   str: The string that is being searched for in self. 
     def increment(self,str):
         WordSet.moveToFront(self.elements, WordSet.showWord(self.elements,str))
         temp = self.elements.pop(0)
